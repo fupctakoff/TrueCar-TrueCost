4de7539c1a910b2077681ca552615ab59d644afc
fupctakoff <egorka1999163@mail.ru>
2023-08-31 15:55:11 +0300

    Delete bags

--- tctc/tctc/settings.py
+++ tctc/tctc/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['87.117.189.41', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['87.117.189.41', '127.0.0.1', 'localhost', '0.0.0.0']
 
 # Application definition
 
